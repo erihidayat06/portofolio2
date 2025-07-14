1867f1c9dd45d00676737dafb58dc529ba70983f
erihidayat06 <erihidayat549@gmail.com>
2025-07-14 08:56:42 +0700

    Tambah logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,63 +3,63 @@
 @section('content')
     <div class="container py-5">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
 
 
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                    <!-- Email Address -->
-                    <div class="mb-3">
-                        <label for="email" class="form-label">{{ __('Email') }}</label>
-                        <input id="email" type="email" name="email"
-                            class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required
-                            autofocus autocomplete="username">
-                        @error('email')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
 
-                    <!-- Password -->
-                    <div class="mb-3">
-                        <label for="password" class="form-label">{{ __('Password') }}</label>
-                        <input id="password" type="password" name="password"
-                            class="form-control @error('password') is-invalid @enderror" required
-                            autocomplete="current-password">
-                        @error('password')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                    <!-- Remember Me -->
-                    <div class="mb-3 form-check">
-                        <input class="form-check-input" type="checkbox" id="remember" name="remember">
-                        <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <label for="email" class="form-label">{{ __('Email') }}</label>
+                    <input id="email" type="email" name="email"
+                        class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required
+                        autofocus autocomplete="username">
+                    @error('email')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                    <div class="d-flex justify-content-between align-items-center">
-                        @if (Route::has('password.request'))
-                            <a class="text-decoration-none" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
+                <!-- Password -->
+                <div class="mb-3">
+                    <label for="password" class="form-label">{{ __('Password') }}</label>
+                    <input id="password" type="password" name="password"
+                        class="form-control @error('password') is-invalid @enderror" required
+                        autocomplete="current-password">
+                    @error('password')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Login') }}
-                        </button>
-                    </div>
+                <!-- Remember Me -->
+                <div class="mb-3 form-check">
+                    <input class="form-check-input" type="checkbox" id="remember" name="remember">
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
+                </div>
 
-                </form>
+                <div class="d-flex justify-content-between align-items-center">
+                    @if (Route::has('password.request'))
+                        <a class="text-decoration-none" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
 
-            </div>
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Login') }}
+                    </button>
+                </div>
+
+            </form>
+
+
         </div>
     </div>
 @endsection
