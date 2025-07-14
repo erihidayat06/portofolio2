b62a0319de60a1820abcdd4a4d4f76235647c35a
erihidayat06 <erihidayat549@gmail.com>
2025-07-14 08:56:02 +0700

    Tambah logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,59 +11,53 @@
                     </div>
                 @endif
 
-                <div class="card shadow-sm">
-                    <div class="card-header text-center">
-                        <h4>{{ __('Login') }}</h4>
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <!-- Email Address -->
+                    <div class="mb-3">
+                        <label for="email" class="form-label">{{ __('Email') }}</label>
+                        <input id="email" type="email" name="email"
+                            class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required
+                            autofocus autocomplete="username">
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
 
-                            <!-- Email Address -->
-                            <div class="mb-3">
-                                <label for="email" class="form-label">{{ __('Email') }}</label>
-                                <input id="email" type="email" name="email"
-                                    class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
-                                    required autofocus autocomplete="username">
-                                @error('email')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <!-- Password -->
-                            <div class="mb-3">
-                                <label for="password" class="form-label">{{ __('Password') }}</label>
-                                <input id="password" type="password" name="password"
-                                    class="form-control @error('password') is-invalid @enderror" required
-                                    autocomplete="current-password">
-                                @error('password')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <!-- Remember Me -->
-                            <div class="mb-3 form-check">
-                                <input class="form-check-input" type="checkbox" id="remember" name="remember">
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
-                                </label>
-                            </div>
-
-                            <div class="d-flex justify-content-between align-items-center">
-                                @if (Route::has('password.request'))
-                                    <a class="text-decoration-none" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-
-                        </form>
+                    <!-- Password -->
+                    <div class="mb-3">
+                        <label for="password" class="form-label">{{ __('Password') }}</label>
+                        <input id="password" type="password" name="password"
+                            class="form-control @error('password') is-invalid @enderror" required
+                            autocomplete="current-password">
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
-                </div>
+
+                    <!-- Remember Me -->
+                    <div class="mb-3 form-check">
+                        <input class="form-check-input" type="checkbox" id="remember" name="remember">
+                        <label class="form-check-label" for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
+                    </div>
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        @if (Route::has('password.request'))
+                            <a class="text-decoration-none" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+
+                </form>
 
             </div>
         </div>
