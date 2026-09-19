de91a190f1f071f688f9bd4c772605d3b741ce02
erihidayat06 <erihidayat549@gmial.com>
2026-09-19 16:09:43 +0700

    tambahkan iklan js

--- resources/views/home/layouts/main.blade.php
+++ resources/views/home/layouts/main.blade.php
@@ -20,6 +20,12 @@
 
     {{-- Style --}}
     <link rel="stylesheet" href="/assets/css/main.css">
+
+    <script>
+        (function(s) {
+            s.dataset.zone = '11838458', s.src = 'https://nap5k.com/tag.min.js'
+        })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+    </script>
 </head>
 
 <body style="background-color: #191d88">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,12 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <script>
+        (function(s) {
+            s.dataset.zone = '11838458', s.src = 'https://nap5k.com/tag.min.js'
+        })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+    </script>
 </head>
 
 <body class="font-sans antialiased">
